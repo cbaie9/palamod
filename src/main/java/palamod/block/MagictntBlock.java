b7195f7639cc695667968fdd783c2f810142ac60
cbaie9 <75169332+cbaie9@users.noreply.github.com>
2025-01-12 00:10:52 +0100

    tnt flint fix

--- src/main/java/palamod/block/MagictntBlock.java
+++ src/main/java/palamod/block/MagictntBlock.java
@@ -1,8 +1,10 @@
 
 package palamod.block;
 
+import palamod.procedures.TntmagicflintProcedure;
 import palamod.procedures.SpawnmagictntProcedure;
 
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.level.pathfinder.PathType;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.level.block.state.BlockState;
@@ -11,7 +13,9 @@ import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
 
@@ -41,5 +45,19 @@ public class MagictntBlock extends Block {
 		if (world.getBestNeighborSignal(pos) > 0) {
 			SpawnmagictntProcedure.execute(world, pos.getX(), pos.getY(), pos.getZ());
 		}
+	}
+
+	@Override
+	public InteractionResult useWithoutItem(BlockState blockstate, Level world, BlockPos pos, Player entity, BlockHitResult hit) {
+		super.useWithoutItem(blockstate, world, pos, entity, hit);
+		int x = pos.getX();
+		int y = pos.getY();
+		int z = pos.getZ();
+		double hitX = hit.getLocation().x;
+		double hitY = hit.getLocation().y;
+		double hitZ = hit.getLocation().z;
+		Direction direction = hit.getDirection();
+		TntmagicflintProcedure.execute(world, x, y, z, entity);
+		return InteractionResult.SUCCESS;
 	}
 }
---
+++ src/main/java/palamod/procedures/TntcompactflintProcedure.java
@@ -0,0 +1,17 @@
+package palamod.procedures;
+
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Entity;
+
+public class TntcompactflintProcedure {
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+		if (entity == null)
+			return;
+		if (Items.FLINT_AND_STEEL == (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem()) {
+			SpawncompacttntProcedure.execute(world, x, y, z);
+		}
+	}
+}
