520c38fe0967cd00dd75e8d370666f8abc5af838
cbaie9 <75169332+cbaie9@users.noreply.github.com>
2025-11-04 19:52:20 +0100

    pass spawner 0.002

--- src/main/java/palamod/procedures/SpawnerupbreakblockProcedure.java
+++ src/main/java/palamod/procedures/SpawnerupbreakblockProcedure.java
@@ -3,8 +3,6 @@ package palamod.procedures;
 import palamod.init.PalamodModItems;
 import palamod.init.PalamodModBlocks;
 
-import palamod.PalamodMod;
-
 import net.neoforged.neoforge.event.level.BlockEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -14,11 +12,15 @@ import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.Vec2;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.item.component.CustomData;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.network.chat.Component;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.BlockPos;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.CommandSource;
@@ -40,15 +42,67 @@ public class SpawnerupbreakblockProcedure {
 		if (entity == null)
 			return;
 		String data = "";
-		if ((entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY).getItem() == PalamodModItems.CAVERNEHAMMER.get()
-				&& ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == PalamodModBlocks.EMPTYSPAWNER.get().asItem()
-						|| (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == PalamodModItems.EMPTYSPAWNERITEM.get())
-				&& Blocks.SPAWNER == (world.getBlockState(BlockPos.containing(x, y, z))).getBlock()) {
+		ItemStack spawner_item = ItemStack.EMPTY;
+		boolean passsetup = false;
+		if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == PalamodModItems.CAVERNEHAMMER.get() && Blocks.SPAWNER == (world.getBlockState(BlockPos.containing(x, y, z))).getBlock()) {
+			if ((entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY).getItem() == PalamodModBlocks.EMPTYSPAWNER.get().asItem()) {
+				spawner_item = new ItemStack(PalamodModItems.EMPTYSPAWNERITEM.get()).copy();
+				passsetup = true;
+			} else if ((entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY).getItem() == PalamodModItems.EMPTYSPAWNERITEM.get()) {
+				spawner_item = (entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY).copy();
+			}
 			data = executeCommandGetResult(world, new Vec3(x, y, z), "data get block ~ ~ ~ SpawnData.entity");
-			if (data.contains("witch")) {
-				PalamodMod.LOGGER.info(data);
-			} else if (data.contains("zombie")) {
-				PalamodMod.LOGGER.info(data);
+			if (data.contains("witch") && (passsetup || (spawner_item.getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getString("spawner_type")).equals("witch"))) {
+				{
+					final String _tagName = "spawner_type";
+					final String _tagValue = "witch";
+					CustomData.update(DataComponents.CUSTOM_DATA, spawner_item, tag -> tag.putString(_tagName, _tagValue));
+				}
+				{
+					final String _tagName = "soul";
+					final double _tagValue = (spawner_item.getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getDouble("souls") + 0);
+					CustomData.update(DataComponents.CUSTOM_DATA, spawner_item, tag -> tag.putDouble(_tagName, _tagValue));
+				}
+			} else if (data.contains("zombie") && (passsetup || (spawner_item.getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getString("spawner_type")).equals("zombie"))) {
+				{
+					final String _tagName = "spawner_type";
+					final String _tagValue = "zombie";
+					CustomData.update(DataComponents.CUSTOM_DATA, spawner_item, tag -> tag.putString(_tagName, _tagValue));
+				}
+				{
+					final String _tagName = "soul";
+					final double _tagValue = (spawner_item.getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getDouble("souls") + 0);
+					CustomData.update(DataComponents.CUSTOM_DATA, spawner_item, tag -> tag.putDouble(_tagName, _tagValue));
+				}
+			} else if (data.contains("skeleton") && (passsetup || (spawner_item.getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getString("spawner_type")).equals("creeper"))) {
+				{
+					final String _tagName = "spawner_type";
+					final String _tagValue = "skeleton";
+					CustomData.update(DataComponents.CUSTOM_DATA, spawner_item, tag -> tag.putString(_tagName, _tagValue));
+				}
+				{
+					final String _tagName = "soul";
+					final double _tagValue = (spawner_item.getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getDouble("souls") + 0);
+					CustomData.update(DataComponents.CUSTOM_DATA, spawner_item, tag -> tag.putDouble(_tagName, _tagValue));
+				}
+			} else if (data.contains("creeper") && (passsetup || (spawner_item.getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getString("spawner_type")).equals("creeper"))) {
+				{
+					final String _tagName = "spawner_type";
+					final String _tagValue = "creeper";
+					CustomData.update(DataComponents.CUSTOM_DATA, spawner_item, tag -> tag.putString(_tagName, _tagValue));
+				}
+				{
+					final String _tagName = "soul";
+					final double _tagValue = (spawner_item.getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getDouble("souls") + 0);
+					CustomData.update(DataComponents.CUSTOM_DATA, spawner_item, tag -> tag.putDouble(_tagName, _tagValue));
+				}
+			}
+			if (entity instanceof LivingEntity _entity) {
+				ItemStack _setstack = spawner_item.copy();
+				_setstack.setCount(1);
+				_entity.setItemInHand(InteractionHand.OFF_HAND, _setstack);
+				if (_entity instanceof Player _player)
+					_player.getInventory().setChanged();
 			}
 		}
 	}
