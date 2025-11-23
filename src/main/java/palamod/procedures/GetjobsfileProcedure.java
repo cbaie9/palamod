5c11668f560abd1e8fd8902d17f3f9e947613c96
cbaie9 <75169332+cbaie9@users.noreply.github.com>
2025-11-23 13:04:18 +0100

    defix getfilejobs

--- src/main/java/palamod/procedures/GetjobsfileProcedure.java
+++ src/main/java/palamod/procedures/GetjobsfileProcedure.java
@@ -1,33 +1,27 @@
 package palamod.procedures;
 
 import palamod.PalamodMod;
+
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.entity.Entity;
+
 import java.io.File;
 
 public class GetjobsfileProcedure {
-    public static File execute(LevelAccessor world, double x, double y, double z, Entity entity) {
-        if (entity == null)
-            return new File("");
-        
-        File output = new File("");
-        
-        try {
-            if (world.isClientSide()) {
-                // Côté client
-                output = ReadjobsclientProcedure.execute(world, entity);
-                PalamodMod.LOGGER.debug("Palamod: Reading client side jobs file");
-            } else {
-                // Côté serveur
-                output = ReadjobsserverProcedure.execute(entity);
-                PalamodMod.LOGGER.debug("Palamod: Reading server side jobs file");
-            }
-        } catch (Exception e) {
-            PalamodMod.LOGGER.error("Palamod ERROR: Failed to get jobs file - " + e.getMessage());
-            // Retourner un fichier par défaut plutôt que de crasher
-            output = new File("palamod_jobs/backup_" + entity.getUUID().toString() + ".json");
-        }
-        
-        return output;
-    }
+	public static File execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+		if (entity == null)
+			return new File("");
+		File output = new File("");
+		if (IsgameserversideProcedure.execute()) {
+			output = ReadjobsserverProcedure.execute(entity);
+			PalamodMod.LOGGER.debug("read server side jobs");
+		} else if (IsgameclientsideProcedure.execute(world, x, y, z)) {
+			output = ReadjobsclientProcedure.execute(world, entity);
+			PalamodMod.LOGGER.debug("read client side jobs");
+		} else {
+			output = ReadjobsserverProcedure.execute(entity);
+			PalamodMod.LOGGER.fatal("FATAL ERROR : jobs file don't exist");
+		}
+		return output;
+	}
 }
