69dec55eed56a33c18f13a1214caf64c5d33aac2
cbaie9 <75169332+cbaie9@users.noreply.github.com>
2025-12-14 18:24:23 +0100

    1.20.057.2
    adding more item to smelt into grinder

--- src/main/java/palamod/client/gui/CrusherguiScreen.java
+++ src/main/java/palamod/client/gui/CrusherguiScreen.java
@@ -51,11 +51,6 @@ public class CrusherguiScreen extends AbstractContainerScreen<CrusherguiMenu> im
 	}
 
 	@Override
-	public boolean isPauseScreen() {
-		return true;
-	}
-
-	@Override
 	public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks) {
 		super.render(guiGraphics, mouseX, mouseY, partialTicks);
 		this.renderTooltip(guiGraphics, mouseX, mouseY);
--- src/main/java/palamod/procedures/GrinderresultinputnumProcedure.java
+++ src/main/java/palamod/procedures/GrinderresultinputnumProcedure.java
@@ -4,29 +4,38 @@ import palamod.init.PalamodModItems;
 import palamod.init.PalamodModBlocks;
 
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.resources.ResourceLocation;
 
 public class GrinderresultinputnumProcedure {
 	public static double execute(ItemStack input_grinder) {
 		ItemStack input = ItemStack.EMPTY;
 		double output = 0;
 		input = input_grinder.copy();
-		if (input.getItem() == PalamodModItems.PALADIUM_INGOT.get()) {
+		if (input.is(ItemTags.create(ResourceLocation.parse("palamod:grinder_type_armor_chestplate")))) {
+			output = 8;
+		} else if (input.is(ItemTags.create(ResourceLocation.parse("palamod:grinder_type_armor_helmet")))) {
+			output = 5;
+		} else if (input.is(ItemTags.create(ResourceLocation.parse("palamod:grinder_type_armor_boots")))) {
+			output = 4;
+		} else if (input.is(ItemTags.create(ResourceLocation.parse("palamod:grinder_type_armor_legging")))) {
+			output = 7;
+		} else if (input.is(ItemTags.create(ResourceLocation.parse("palamod:grinder_type_ingot")))) {
 			output = 1;
-		} else if (input.getItem() == PalamodModBlocks.PALADIUM_BLOCK.get().asItem()) {
-			output = 9;
-		} else if (input.getItem() == PalamodModItems.PALADIUMRAWORE.get()) {
-			output = 1;
-		} else if (input.getItem() == PalamodModItems.GREEN_PALADIUM_INGOT.get()) {
-			output = 1;
-		} else if (input.getItem() == PalamodModBlocks.PALADIUM_GREEN_BLOCK.get().asItem()) {
-			output = 9;
-		} else if (input.getItem() == PalamodModItems.GREENPALADIUMRAWORE.get()) {
-			output = 1;
-		} else if (input.getItem() == PalamodModItems.ENDIUM_NUGGET.get()) {
-			output = 1;
-		} else if (input.getItem() == PalamodModItems.ENDIUM_INGOT.get()) {
+		} else if (input.is(ItemTags.create(ResourceLocation.parse("palamod:grinder_type_tool_cost2")))) {
+			output = 2;
+		} else if (input.is(ItemTags.create(ResourceLocation.parse("palamod:grinder_type_tool_cost3")))) {
+			output = 3;
+		} else if (input.is(ItemTags.create(ResourceLocation.parse("palamod:grinder_type_tool_cost6")))) {
+			output = 6;
+		} else if (input.is(ItemTags.create(ResourceLocation.parse("palamod:grinder_type_block")))) {
 			output = 9;
 		} else if (input.getItem() == PalamodModBlocks.ENDIUM_BLOCK.get().asItem()) {
+			output = 81;
+		} else if (input.getItem() == PalamodModItems.ENDIUM_SWORD.get() || input.getItem() == PalamodModItems.ENDIUM_HOE.get() || input.getItem() == PalamodModItems.HEADENDIUMSWORD.get()) {
+			output = 18;
+		} else if (input.getItem() == PalamodModItems.ENDIUM_PICKAXE.get() || input.getItem() == PalamodModItems.ENDIUM_AXE.get() || input.getItem() == PalamodModItems.HEAD_ENDIUM_PICKAXE.get()
+				|| input.getItem() == PalamodModItems.HEAD_ENDIUM_AXE.get()) {
 			output = 27;
 		}
 		return output;
