744a99da88be5d89ca1be09532464a597ed088d7
cbaie9 <75169332+cbaie9@users.noreply.github.com>
2025-12-16 22:56:57 +0100

    remove jei api + div/4 trixium gen

---
+++ src/main/java/palamod/item/SpawnerupgradelootingItem.java
@@ -0,0 +1,9 @@
+package palamod.item;
+
+import net.minecraft.world.item.Item;
+
+public class SpawnerupgradelootingItem extends Item {
+	public SpawnerupgradelootingItem() {
+		super(new Item.Properties().stacksTo(1));
+	}
+}
---
+++ src/main/java/palamod/item/SpawnerupgrademoreItem.java
@@ -0,0 +1,9 @@
+package palamod.item;
+
+import net.minecraft.world.item.Item;
+
+public class SpawnerupgrademoreItem extends Item {
+	public SpawnerupgrademoreItem() {
+		super(new Item.Properties().stacksTo(1));
+	}
+}
