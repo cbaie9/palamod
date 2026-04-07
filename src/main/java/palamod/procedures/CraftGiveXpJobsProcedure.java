dfbd51b35947d5e3264bac3525a1d181446fe40a
cbaie9 <75169332+cbaie9@users.noreply.github.com>
2026-04-07 23:32:08 +0200

    GiveXpOnCraft pt1

---
+++ src/main/java/palamod/procedures/CraftGiveXpJobsProcedure.java
@@ -0,0 +1,155 @@
+package palamod.procedures;
+
+import palamod.network.PalamodModVariables;
+
+import palamod.init.PalamodModItems;
+import palamod.init.PalamodModGameRules;
+
+import palamod.PalamodMod;
+
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.GameType;
+import net.minecraft.world.item.component.CustomData;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.network.chat.Component;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.core.BlockPos;
+import net.minecraft.client.multiplayer.PlayerInfo;
+import net.minecraft.client.Minecraft;
+
+import java.io.IOException;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.File;
+import java.io.BufferedReader;
+
+public class CraftGiveXpJobsProcedure {
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity, ItemStack item_craft) {
+		if (entity == null)
+			return;
+		com.google.gson.JsonObject main = new com.google.gson.JsonObject();
+		ItemStack item = ItemStack.EMPTY;
+		File jobs = new File("");
+		File money = new File("");
+		String jobs_string = "";
+		double xp_receive = 0;
+		double jobs_type_xpbottle = 0;
+		item = item_craft.copy();
+		if (!world.getLevelData().getGameRules().getBoolean(PalamodModGameRules.DISABLEJOBSGAMERULE)) {
+			jobs = ReadjobsserverProcedure.execute(entity);
+			money = ReadMoneyFileProcedure.execute(entity);
+			RecalcJobsXpBaseMultiplierProcedure.execute(world, entity);
+			if (jobs.exists() && !(getEntityGameType(entity) == GameType.CREATIVE) && money.exists()) {
+				{
+					try {
+						BufferedReader bufferedReader = new BufferedReader(new FileReader(PalamodModVariables.server_path_cache));
+						StringBuilder jsonstringbuilder = new StringBuilder();
+						String line;
+						while ((line = bufferedReader.readLine()) != null) {
+							jsonstringbuilder.append(line);
+						}
+						bufferedReader.close();
+						main = new com.google.gson.Gson().fromJson(jsonstringbuilder.toString(), com.google.gson.JsonObject.class);
+						if (world.dayTime() > main.get("xpstreak_time_miner").getAsDouble()) {
+							main.addProperty("xpstreak_miner", 0);
+						}
+						if (world.dayTime() > main.get("xpstreak_time_farmer").getAsDouble()) {
+							main.addProperty("xpstreak_farmer", 0);
+						}
+						if (world.dayTime() > main.get("xpstreak_time_hunter").getAsDouble()) {
+							main.addProperty("xpstreak_hunter", 0);
+						}
+						if (world.dayTime() > main.get("xpstreak_time_alchi").getAsDouble()) {
+							main.addProperty("xpstreak_alchi", 0);
+						}
+						xp_receive = GetXpcraftjobsProcedure.execute(entity, item_craft);
+						if (0 < xp_receive) {
+							if (item.is(ItemTags.create(ResourceLocation.parse("palamod:farmer_jobs")))) {
+								jobs_string = "farmer";
+								jobs_type_xpbottle = 2;
+							} else if (item.is(ItemTags.create(ResourceLocation.parse("palamod:alchimist_jobs")))) {
+								jobs_string = "alchi";
+								jobs_type_xpbottle = 4;
+							} else if (item.is(ItemTags.create(ResourceLocation.parse("palamod:miner_jobs")))) {/*No craft are in the palamod in paladium here, futurproofing*/
+								jobs_string = "miner";
+								jobs_type_xpbottle = 1;
+							} else if (item.is(ItemTags.create(ResourceLocation.parse("palamod:hunter_jobs")))) {/*No craft are in the palamod in paladium here, futurproofing*/
+								jobs_string = "hunter";
+								jobs_type_xpbottle = 3;
+							} else {
+								jobs_string = "alchi";
+								PalamodMod.LOGGER.error(("[ Palamod ][ from craftgivejobs.java] : error -> Craft with approved xp amount but no attribued jobs | item :  '" + item + "' , fallback to alchimist"));
+							}
+							if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY)
+									.getEnchantmentLevel(world.registryAccess().lookupOrThrow(Registries.ENCHANTMENT).getOrThrow(ResourceKey.create(Registries.ENCHANTMENT, ResourceLocation.parse("palamod:botteled")))) != 0
+									&& (0 == (entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY).getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getDouble("jobs_type")
+											|| 1 == (entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY).getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getDouble("jobs_type"))
+									&& (entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY).getItem() == PalamodModItems.XP_BOTTLE.get()) {
+								{
+									final String _tagName = "xp_jobs";
+									final double _tagValue = (xp_receive * main.get("multi_exp").getAsDouble()
+											+ (entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY).getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getDouble("xp_jobs"));
+									CustomData.update(DataComponents.CUSTOM_DATA, (entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY), tag -> tag.putDouble(_tagName, _tagValue));
+								}
+								{
+									final String _tagName = "jobs_type";
+									final double _tagValue = jobs_type_xpbottle;
+									CustomData.update(DataComponents.CUSTOM_DATA, (entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY), tag -> tag.putDouble(_tagName, _tagValue));
+								}
+							} else {
+								main.addProperty(("xp_" + jobs_string), (xp_receive * main.get("multi_exp").getAsDouble() + main.get(("xp_" + jobs_string)).getAsDouble()));
+							}
+							main.addProperty(("xpstreak_" + jobs_string), (xp_receive * main.get("multi_exp").getAsDouble() + main.get(("xpstreak_" + jobs_string)).getAsDouble()));
+							main.addProperty(("xpstreak_time_" + jobs_string), (world.dayTime() + 80));
+							if (entity instanceof Player _player && !_player.level().isClientSide())
+								_player.displayClientMessage(
+										Component
+												.literal(
+														(Component.translatable("palamod.procedure.jobswin1").getString() + "" + (xp_receive * main.get("multi_exp").getAsDouble() + main.get(("xpstreak_" + jobs_string)).getAsDouble())
+																+ Component.translatable("palamod.procedure.jobswin2").getString() + " "
+																+ Component
+																		.translatable(((BuiltInRegistries.BLOCK.getKey((world.getBlockState(BlockPos.containing(x, y, z))).getBlock()).toString()).replace("minecraft:",
+																				(world.getBlockState(BlockPos.containing(x, y, z))).is(BlockTags.create(ResourceLocation.parse("palamod:palablocks"))) ? "block.palamod." : "block.minecraft.")))
+																		.getString())),
+										true);
+						}
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+				{
+					com.google.gson.Gson mainGSONBuilderVariable = new com.google.gson.GsonBuilder().setPrettyPrinting().create();
+					try {
+						FileWriter fileWriter = new FileWriter(jobs);
+						fileWriter.write(mainGSONBuilderVariable.toJson(main));
+						fileWriter.close();
+					} catch (IOException exception) {
+						exception.printStackTrace();
+					}
+				}
+				ChecklvlminerProcedure.execute(world, x, y, z, entity);
+			}
+		}
+	}
+
+	private static GameType getEntityGameType(Entity entity) {
+		if (entity instanceof ServerPlayer serverPlayer) {
+			return serverPlayer.gameMode.getGameModeForPlayer();
+		} else if (entity instanceof Player player && player.level().isClientSide()) {
+			PlayerInfo playerInfo = Minecraft.getInstance().getConnection().getPlayerInfo(player.getGameProfile().getId());
+			if (playerInfo != null)
+				return playerInfo.getGameMode();
+		}
+		return null;
+	}
+}
---
+++ src/main/java/palamod/procedures/GetXpcraftjobsProcedure.java
@@ -0,0 +1,73 @@
+package palamod.procedures;
+
+import palamod.init.PalamodModItems;
+import palamod.init.PalamodModBlocks;
+
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.resources.ResourceLocation;
+
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.File;
+import java.io.BufferedReader;
+
+public class GetXpcraftjobsProcedure {
+	public static double execute(Entity entity, ItemStack item_craft) {
+		if (entity == null)
+			return 0;
+		File cache = new File("");
+		File jobs = new File("");
+		BlockState block = Blocks.AIR.defaultBlockState();
+		com.google.gson.JsonObject jobs_main = new com.google.gson.JsonObject();
+		com.google.gson.JsonObject cache_main = new com.google.gson.JsonObject();
+		ItemStack item = ItemStack.EMPTY;
+		double output = 0;
+		double blockstate_value = 0;
+		double nloop = 0;
+		double lvl = 0;
+		double lvl_farmer = 0;
+		double lvl_hunter = 0;
+		double lvl_alchi = 0;
+		item = item_craft.copy();
+		jobs = GetjobsfileProcedure.execute(entity);
+		if (cache.exists() && jobs.exists()) {
+			{
+				try {
+					BufferedReader bufferedReader = new BufferedReader(new FileReader(jobs));
+					StringBuilder jsonstringbuilder = new StringBuilder();
+					String line;
+					while ((line = bufferedReader.readLine()) != null) {
+						jsonstringbuilder.append(line);
+					}
+					bufferedReader.close();
+					jobs_main = new com.google.gson.Gson().fromJson(jsonstringbuilder.toString(), com.google.gson.JsonObject.class);
+					lvl = jobs_main.get("lvl_miner").getAsDouble();
+					lvl_farmer = jobs_main.get("lvl_farmer").getAsDouble();
+					lvl_hunter = jobs_main.get("lvl_hunter").getAsDouble();
+					lvl_alchi = jobs_main.get("lvl_alchi").getAsDouble();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if (item.getItem() == PalamodModItems.FLASK.get()) {
+				output = 0.2;
+			} else if (item.getItem() == PalamodModBlocks.EXTRACTOR.get().asItem()) {
+				output = 20;
+			} else if (item.is(ItemTags.create(ResourceLocation.parse("palamod:glueball_tier_one"))) && lvl_alchi >= 6) {
+				output = 2;
+			} else if (item.is(ItemTags.create(ResourceLocation.parse("palamod:glueball_tier_two"))) && lvl_alchi >= 10) {
+				output = 15;
+			} else if (item.getItem() == Items.BREAD) {
+				output = 1;
+			} else if (item.getItem() == Items.PUMPKIN_PIE && lvl_farmer >= 11) {
+				output = 4;
+			}
+		}
+		return output;
+	}
+}
