722f6c62e5138b562092e374e0afabf73b8d7943
cbaie9 <75169332+cbaie9@users.noreply.github.com>
2026-04-12 19:09:44 +0200

    tank code finished

--- src/main/java/palamod/block/AmethysttankBlock.java
+++ src/main/java/palamod/block/AmethysttankBlock.java
@@ -1,6 +1,7 @@
 package palamod.block;
 
 import palamod.procedures.TanksetupProcedure;
+import palamod.procedures.TankprocessProcedure;
 
 import palamod.block.entity.AmethysttankBlockEntity;
 
@@ -9,6 +10,7 @@ import org.checkerframework.checker.units.qual.s;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.block.state.StateDefinition;
@@ -20,8 +22,11 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.MenuProvider;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.Containers;
+import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
 
 public class AmethysttankBlock extends Block implements EntityBlock {
@@ -120,7 +125,21 @@ public class AmethysttankBlock extends Block implements EntityBlock {
 	@Override
 	public void onPlace(BlockState blockstate, Level world, BlockPos pos, BlockState oldState, boolean moving) {
 		super.onPlace(blockstate, world, pos, oldState, moving);
-		TanksetupProcedure.execute(world, pos.getX(), pos.getY(), pos.getZ());
+		TanksetupProcedure.execute();
+	}
+
+	@Override
+	public InteractionResult useWithoutItem(BlockState blockstate, Level world, BlockPos pos, Player entity, BlockHitResult hit) {
+		super.useWithoutItem(blockstate, world, pos, entity, hit);
+		int x = pos.getX();
+		int y = pos.getY();
+		int z = pos.getZ();
+		double hitX = hit.getLocation().x;
+		double hitY = hit.getLocation().y;
+		double hitZ = hit.getLocation().z;
+		Direction direction = hit.getDirection();
+		TankprocessProcedure.execute(world, x, y, z, entity);
+		return InteractionResult.SUCCESS;
 	}
 
 	@Override
---
+++ src/main/java/palamod/procedures/TankprocessProcedure.java
@@ -0,0 +1,216 @@
+package palamod.procedures;
+
+import palamod.init.PalamodModItems;
+import palamod.init.PalamodModGameRules;
+import palamod.init.PalamodModBlocks;
+
+import palamod.PalamodMod;
+
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.component.CustomData;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.network.chat.Component;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.core.BlockPos;
+
+public class TankprocessProcedure {
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+		if (entity == null)
+			return;
+		String seve_string = "";
+		double max_stock = 0;
+		double decalc_blockstate = 0;
+		if (world.getLevelData().getGameRules().getBoolean(PalamodModGameRules.PALAMODDEBUGLOG)) {
+			PalamodMod.LOGGER.debug(("---------------------" + "\n" + "tank" + "\n" + "stock : " + getBlockNBTNumber(world, BlockPos.containing(x, y, z), "stock") + "\n" + "type : " + getBlockNBTString(world, BlockPos.containing(x, y, z), "type")));
+		}
+		if (!getBlockNBTLogic(world, BlockPos.containing(x, y, z), "setup")) {
+			if (!world.isClientSide()) {
+				BlockPos _bp = BlockPos.containing(x, y, z);
+				BlockEntity _blockEntity = world.getBlockEntity(_bp);
+				BlockState _bs = world.getBlockState(_bp);
+				if (_blockEntity != null) {
+					_blockEntity.getPersistentData().putBoolean("setup", true);
+					_blockEntity.getPersistentData().putDouble("stock", 0);
+					_blockEntity.getPersistentData().putString("type", "None");
+				}
+				if (world instanceof Level _level)
+					_level.sendBlockUpdated(_bp, _bs, _bs, 3);
+			}
+		}
+		if (1 == (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getDouble("type")) {
+			seve_string = "ostrya";
+		} else if (2 == (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getDouble("type")) {
+			seve_string = "judeecercis";
+		} else if (3 == (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getDouble("type")) {
+			seve_string = "jacaranda";
+		} else if (4 == (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getDouble("type")) {
+			seve_string = "erable";
+		} else {
+			seve_string = "NTOSI_error";
+		}
+		if ((world.getBlockState(BlockPos.containing(x, y, z))).getBlock() == PalamodModBlocks.AMETHYSTTANK.get()) {/*amethyst*/
+			max_stock = 60;
+		} else if ((world.getBlockState(BlockPos.containing(x, y, z))).getBlock() == PalamodModBlocks.AMETHYSTTANK.get()) {/*gold*/
+			max_stock = 35;
+		} else if ((world.getBlockState(BlockPos.containing(x, y, z))).getBlock() == PalamodModBlocks.AMETHYSTTANK.get()) {/*titane*/
+			max_stock = 95;
+		} else if ((world.getBlockState(BlockPos.containing(x, y, z))).getBlock() == PalamodModBlocks.AMETHYSTTANK.get()) {/*paladium*/
+			max_stock = 135;
+		}
+		if (PalamodModItems.FLASK.get() == (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem()) {
+			if (world.getLevelData().getGameRules().getBoolean(PalamodModGameRules.PALAMODDEBUGLOG)) {
+				PalamodMod.LOGGER.info((((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getDouble("seve")) + " - " + seve_string));
+			}
+			if (0 < (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getDouble("seve")) {
+				PalamodMod.LOGGER.info((getBlockNBTString(world, BlockPos.containing(x, y, z), "type") + " -  block"));
+				if ((seve_string).equals(getBlockNBTString(world, BlockPos.containing(x, y, z), "type")) || ("None").equals(getBlockNBTString(world, BlockPos.containing(x, y, z), "type"))) {
+					if (max_stock >= getBlockNBTNumber(world, BlockPos.containing(x, y, z), "stock")
+							+ (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getDouble("seve")) {
+						if (!world.isClientSide()) {
+							BlockPos _bp = BlockPos.containing(x, y, z);
+							BlockEntity _blockEntity = world.getBlockEntity(_bp);
+							BlockState _bs = world.getBlockState(_bp);
+							if (_blockEntity != null) {
+								_blockEntity.getPersistentData().putDouble("stock", (getBlockNBTNumber(world, BlockPos.containing(x, y, z), "stock")
+										+ (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getDouble("seve")));
+							}
+							if (world instanceof Level _level)
+								_level.sendBlockUpdated(_bp, _bs, _bs, 3);
+						}
+						{
+							final String _tagName = "seve";
+							final double _tagValue = 0;
+							CustomData.update(DataComponents.CUSTOM_DATA, (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY), tag -> tag.putDouble(_tagName, _tagValue));
+						}
+						if (!world.isClientSide()) {
+							BlockPos _bp = BlockPos.containing(x, y, z);
+							BlockEntity _blockEntity = world.getBlockEntity(_bp);
+							BlockState _bs = world.getBlockState(_bp);
+							if (_blockEntity != null) {
+								_blockEntity.getPersistentData().putString("type", seve_string);
+							}
+							if (world instanceof Level _level)
+								_level.sendBlockUpdated(_bp, _bs, _bs, 3);
+						}
+					} else {
+						if (!world.isClientSide()) {
+							BlockPos _bp = BlockPos.containing(x, y, z);
+							BlockEntity _blockEntity = world.getBlockEntity(_bp);
+							BlockState _bs = world.getBlockState(_bp);
+							if (_blockEntity != null) {
+								_blockEntity.getPersistentData().putDouble("stock", max_stock);
+							}
+							if (world instanceof Level _level)
+								_level.sendBlockUpdated(_bp, _bs, _bs, 3);
+						}
+						{
+							final String _tagName = "seve";
+							final double _tagValue = ((getBlockNBTNumber(world, BlockPos.containing(x, y, z), "stock")
+									+ (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag().getDouble("seve")) - max_stock);
+							CustomData.update(DataComponents.CUSTOM_DATA, (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY), tag -> tag.putDouble(_tagName, _tagValue));
+						}
+					}
+				}
+			} else {
+				if (0 < getBlockNBTNumber(world, BlockPos.containing(x, y, z), "stock")) {
+					if (getBlockNBTNumber(world, BlockPos.containing(x, y, z), "stock") <= 15) {
+						{
+							final String _tagName = "seve";
+							final double _tagValue = (getBlockNBTNumber(world, BlockPos.containing(x, y, z), "stock"));
+							CustomData.update(DataComponents.CUSTOM_DATA, (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY), tag -> tag.putDouble(_tagName, _tagValue));
+						}
+						if (!world.isClientSide()) {
+							BlockPos _bp = BlockPos.containing(x, y, z);
+							BlockEntity _blockEntity = world.getBlockEntity(_bp);
+							BlockState _bs = world.getBlockState(_bp);
+							if (_blockEntity != null) {
+								_blockEntity.getPersistentData().putDouble("stock", 0);
+							}
+							if (world instanceof Level _level)
+								_level.sendBlockUpdated(_bp, _bs, _bs, 3);
+						}
+					} else {
+						{
+							final String _tagName = "seve";
+							final double _tagValue = 15;
+							CustomData.update(DataComponents.CUSTOM_DATA, (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY), tag -> tag.putDouble(_tagName, _tagValue));
+						}
+						if (!world.isClientSide()) {
+							BlockPos _bp = BlockPos.containing(x, y, z);
+							BlockEntity _blockEntity = world.getBlockEntity(_bp);
+							BlockState _bs = world.getBlockState(_bp);
+							if (_blockEntity != null) {
+								_blockEntity.getPersistentData().putDouble("stock", (getBlockNBTNumber(world, BlockPos.containing(x, y, z), "stock") - 15));
+							}
+							if (world instanceof Level _level)
+								_level.sendBlockUpdated(_bp, _bs, _bs, 3);
+						}
+					}
+				}
+			}
+		} else {
+			if (entity instanceof Player _player && !_player.level().isClientSide())
+				_player.displayClientMessage(Component.literal((BuiltInRegistries.ITEM.getKey((new ItemStack((world.getBlockState(BlockPos.containing(x, y, z))).getBlock())).getItem()).toString() + " - "
+						+ getBlockNBTNumber(world, BlockPos.containing(x, y, z), "stock") + "/" + max_stock + " | Type : " + getBlockNBTString(world, BlockPos.containing(x, y, z), "type"))), false);
+		} /*reload blockstate*/
+		if (("jacaranda").equals(getBlockNBTString(world, BlockPos.containing(x, y, z), "type"))) {
+			decalc_blockstate = 0;
+		} else if (("erable").equals(getBlockNBTString(world, BlockPos.containing(x, y, z), "type"))) {
+			decalc_blockstate = 7;
+		} else if (("judeecercis").equals(getBlockNBTString(world, BlockPos.containing(x, y, z), "type"))) {
+			decalc_blockstate = 14;
+		} else if (("ostrya").equals(getBlockNBTString(world, BlockPos.containing(x, y, z), "type"))) {
+			decalc_blockstate = 21;
+		}
+		if (getBlockNBTNumber(world, BlockPos.containing(x, y, z), "stock") > 0) {
+			{
+				int _value = (int) ((getBlockNBTNumber(world, BlockPos.containing(x, y, z), "stock") / max_stock) * 7 + decalc_blockstate);
+				BlockPos _pos = BlockPos.containing(x, y, z);
+				BlockState _bs = world.getBlockState(_pos);
+				if (_bs.getBlock().getStateDefinition().getProperty("blockstate") instanceof IntegerProperty _integerProp && _integerProp.getPossibleValues().contains(_value))
+					world.setBlock(_pos, _bs.setValue(_integerProp, _value), 3);
+			}
+		} else {
+			{
+				int _value = 0;
+				BlockPos _pos = BlockPos.containing(x, y, z);
+				BlockState _bs = world.getBlockState(_pos);
+				if (_bs.getBlock().getStateDefinition().getProperty("blockstate") instanceof IntegerProperty _integerProp && _integerProp.getPossibleValues().contains(_value))
+					world.setBlock(_pos, _bs.setValue(_integerProp, _value), 3);
+			}
+		}
+		if (world.getLevelData().getGameRules().getBoolean(PalamodModGameRules.PALAMODDEBUGLOG)) {
+			PalamodMod.LOGGER.debug(("fin-tank V2" + "\n" + "tank" + "\n" + "stock : " + getBlockNBTNumber(world, BlockPos.containing(x, y, z), "stock") + "\n" + "BLOCKSTATE : "
+					+ ((getBlockNBTNumber(world, BlockPos.containing(x, y, z), "stock") / max_stock) * 7 + decalc_blockstate) + "\n" + "fin-tank"));
+		}
+	}
+
+	private static double getBlockNBTNumber(LevelAccessor world, BlockPos pos, String tag) {
+		BlockEntity blockEntity = world.getBlockEntity(pos);
+		if (blockEntity != null)
+			return blockEntity.getPersistentData().getDouble(tag);
+		return -1;
+	}
+
+	private static String getBlockNBTString(LevelAccessor world, BlockPos pos, String tag) {
+		BlockEntity blockEntity = world.getBlockEntity(pos);
+		if (blockEntity != null)
+			return blockEntity.getPersistentData().getString(tag);
+		return "";
+	}
+
+	private static boolean getBlockNBTLogic(LevelAccessor world, BlockPos pos, String tag) {
+		BlockEntity blockEntity = world.getBlockEntity(pos);
+		if (blockEntity != null)
+			return blockEntity.getPersistentData().getBoolean(tag);
+		return false;
+	}
+}
