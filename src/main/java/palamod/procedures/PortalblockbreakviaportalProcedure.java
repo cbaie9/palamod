9b935d7d582845dabe546b197953af3fe5b50e02
cbaie9 <75169332+cbaie9@users.noreply.github.com>
2026-05-02 14:56:58 +0200

    fix portal break

--- src/main/java/palamod/procedures/PortalblockbreakviaportalProcedure.java
+++ src/main/java/palamod/procedures/PortalblockbreakviaportalProcedure.java
@@ -6,8 +6,13 @@ import palamod.init.PalamodModBlocks;
 import palamod.PalamodMod;
 
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.core.BlockPos;
 
 public class PortalblockbreakviaportalProcedure {
@@ -62,10 +67,94 @@ public class PortalblockbreakviaportalProcedure {
 							if (pass) {
 								break;
 							}
+							loop = loop + 1;
 						}
 					}
 				}
 			}
 		}
+		if (pass && !CheckportalstructureProcedure.execute(world, z_core, y_core, x_core, angle_block, shiny_wood)) {
+			if ((getBlockNBTString(world, BlockPos.containing(x_core, y_core, z_core), "position")).equals("west")) {
+				world.destroyBlock(BlockPos.containing(x_core + 1, y_core, z_core + 1), false);
+				world.destroyBlock(BlockPos.containing(x_core + 1, y_core, z_core), false);
+				world.destroyBlock(BlockPos.containing(x_core + 1, y_core, z_core - 1), false);
+				world.destroyBlock(BlockPos.containing(x_core + 2, y_core, z_core + 1), false);
+				world.destroyBlock(BlockPos.containing(x_core + 2, y_core, z_core - 1), false);
+				world.destroyBlock(BlockPos.containing(x_core + 2, y_core, z_core), false);
+				world.destroyBlock(BlockPos.containing(x_core + 3, y_core, z_core + 1), false);
+				world.destroyBlock(BlockPos.containing(x_core + 3, y_core, z_core), false);
+				world.destroyBlock(BlockPos.containing(x_core + 3, y_core, z_core - 1), false);
+			} else if ((getBlockNBTString(world, BlockPos.containing(x_core, y_core, z_core), "position")).equals("east")) {
+				world.destroyBlock(BlockPos.containing(x_core - 1, y_core, z_core + 1), false);
+				world.destroyBlock(BlockPos.containing(x_core - 1, y_core, z_core), false);
+				world.destroyBlock(BlockPos.containing(x_core - 1, y_core, z_core - 1), false);
+				world.destroyBlock(BlockPos.containing(x_core - 2, y_core, z_core + 1), false);
+				world.destroyBlock(BlockPos.containing(x_core - 2, y_core, z_core - 1), false);
+				world.destroyBlock(BlockPos.containing(x_core - 2, y_core, z_core), false);
+				world.destroyBlock(BlockPos.containing(x_core - 3, y_core, z_core + 1), false);
+				world.destroyBlock(BlockPos.containing(x_core - 3, y_core, z_core), false);
+				world.destroyBlock(BlockPos.containing(x_core - 3, y_core, z_core - 1), false);
+			} else if ((getBlockNBTString(world, BlockPos.containing(x_core, y_core, z_core), "position")).equals("north")) {
+				world.destroyBlock(BlockPos.containing(x_core + 1, y_core, z_core - 1), false);
+				world.destroyBlock(BlockPos.containing(x_core, y_core, z_core - 1), false);
+				world.destroyBlock(BlockPos.containing(x_core - 1, y_core, z_core - 1), false);
+				world.destroyBlock(BlockPos.containing(x_core + 1, y_core, z_core - 2), false);
+				world.destroyBlock(BlockPos.containing(x_core, y_core, z_core - 2), false);
+				world.destroyBlock(BlockPos.containing(x_core - 1, y_core, z_core - 2), false);
+				world.destroyBlock(BlockPos.containing(x_core + 1, y_core, z_core - 3), false);
+				world.destroyBlock(BlockPos.containing(x_core, y_core, z_core - 3), false);
+				world.destroyBlock(BlockPos.containing(x_core - 1, y_core, z_core - 3), false);
+			} else if ((getBlockNBTString(world, BlockPos.containing(x_core, y_core, z_core), "position")).equals("south")) {
+				world.destroyBlock(BlockPos.containing(x_core + 1, y_core, z_core + 1), false);
+				world.destroyBlock(BlockPos.containing(x_core, y_core, z_core + 1), false);
+				world.destroyBlock(BlockPos.containing(x_core - 1, y_core, z_core + 1), false);
+				world.destroyBlock(BlockPos.containing(x_core + 1, y_core, z_core + 2), false);
+				world.destroyBlock(BlockPos.containing(x_core, y_core, z_core + 2), false);
+				world.destroyBlock(BlockPos.containing(x_core - 1, y_core, z_core + 2), false);
+				world.destroyBlock(BlockPos.containing(x_core + 1, y_core, z_core + 3), false);
+				world.destroyBlock(BlockPos.containing(x_core, y_core, z_core + 3), false);
+				world.destroyBlock(BlockPos.containing(x_core - 1, y_core, z_core + 3), false);
+			}
+			if (!world.isClientSide()) {
+				BlockPos _bp = BlockPos.containing(x_core, y_core, z_core);
+				BlockEntity _blockEntity = world.getBlockEntity(_bp);
+				BlockState _bs = world.getBlockState(_bp);
+				if (_blockEntity != null) {
+					_blockEntity.getPersistentData().putBoolean("portal_powered", false);
+				}
+				if (world instanceof Level _level)
+					_level.sendBlockUpdated(_bp, _bs, _bs, 3);
+			}
+			if (world instanceof ServerLevel _level) {
+				ItemEntity entityToSpawn = new ItemEntity(_level, x_core, (y_core + 0.25), z_core, (getBlockNBTItemStack(world, BlockPos.containing(x_core, y_core, z_core), "key")));
+				entityToSpawn.setPickUpDelay(10);
+				_level.addFreshEntity(entityToSpawn);
+			}
+			if (!world.isClientSide()) {
+				BlockPos _bp = BlockPos.containing(x, y, z);
+				BlockEntity _blockEntity = world.getBlockEntity(_bp);
+				BlockState _bs = world.getBlockState(_bp);
+				if (_blockEntity != null) {
+					_blockEntity.getPersistentData().put("key", new ItemStack(Blocks.AIR).saveOptional(world.registryAccess()));
+					_blockEntity.getPersistentData().putString("position", "None");
+				}
+				if (world instanceof Level _level)
+					_level.sendBlockUpdated(_bp, _bs, _bs, 3);
+			}
+		}
+	}
+
+	private static String getBlockNBTString(LevelAccessor world, BlockPos pos, String tag) {
+		BlockEntity blockEntity = world.getBlockEntity(pos);
+		if (blockEntity != null)
+			return blockEntity.getPersistentData().getString(tag);
+		return "";
+	}
+
+	private static ItemStack getBlockNBTItemStack(LevelAccessor world, BlockPos pos, String tag) {
+		BlockEntity blockEntity = world.getBlockEntity(pos);
+		if (blockEntity != null)
+			return ItemStack.parseOptional(world.registryAccess(), blockEntity.getPersistentData().getCompound(tag));
+		return ItemStack.EMPTY;
 	}
 }
