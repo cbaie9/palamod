56577423576404204e70614443974da9532a1471
cbaie9 <75169332+cbaie9@users.noreply.github.com>
2026-05-08 21:14:06 +0200

    oxmod flask command

---
+++ src/main/java/palamod/procedures/OxlibflaskluncherviajacarandaProcedure.java
@@ -0,0 +1,15 @@
+package palamod.procedures;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.commands.CommandSourceStack;
+
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.arguments.DoubleArgumentType;
+
+public class OxlibflaskluncherviajacarandaProcedure {
+	public static void execute(CommandContext<CommandSourceStack> arguments, Entity entity) {
+		if (entity == null)
+			return;
+		OxLibflaskgeneratorProcedure.execute(entity, DoubleArgumentType.getDouble(arguments, "stock"), "jacaranda");
+	}
+}
