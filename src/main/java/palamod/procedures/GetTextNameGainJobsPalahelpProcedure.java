7686499c7ffabd4088e08bd16b3d7962da4bce00
cbaie9 <75169332+cbaie9@users.noreply.github.com>
2026-07-26 15:15:25 +0200

    Fix wrong blockstate on ore and wood

--- src/main/java/palamod/procedures/GetTextNameGainJobsPalahelpProcedure.java
+++ src/main/java/palamod/procedures/GetTextNameGainJobsPalahelpProcedure.java
@@ -32,6 +32,10 @@ public class GetTextNameGainJobsPalahelpProcedure {
 			if ((entity.getPersistentData().getString("xp_mode")).equals("kill")) {
 				xpGain = GetXpcraftjobsentityProcedure.execute(entity, 199, 199, 199, 199);
 			}
+		} else if ((entity.getPersistentData().getString("jobs_mode")).equals("alchi")) {
+			if ((entity.getPersistentData().getString("xp_mode")).equals("craft")) {
+				xpGain = GetXpcraftjobsProcedure.execute(item, 199, 199, 199, 199, "craft");
+			}
 		}
 		return item.getDisplayName().getString() + "\n" + (Component.translatable("palamod.procedure.palahelp.jobs.gain").getString()).replace("%1", "" + Math.round(xpGain));
 	}
